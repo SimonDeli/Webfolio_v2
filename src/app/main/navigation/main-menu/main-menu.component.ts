875d476193ad4dc23d991d472944dd639ce7fa06
Simon Delineau Elocky <simondelineau@gmail.com>
2020-08-21 10:19:00 +0200

    test main menu

--- src/app/main/navigation/main-menu/main-menu.component.ts
+++ src/app/main/navigation/main-menu/main-menu.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { animations } from './animations';
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.component.html',
-  styleUrls: ['./main-menu.component.scss']
+  styleUrls: ['./main-menu.component.scss'],
+  animations: [animations]
 })
 export class MainMenuComponent implements OnInit {
+
+  menuShow:boolean = false;
+  barMenuShow:boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  toggle() : void {
+    this.menuShow = !this.menuShow;
+  }
 }
